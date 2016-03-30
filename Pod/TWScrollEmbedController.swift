688148b0371dfa33ab6476beaa0f390f0ecbd1fb
taewan0530 <kimtaewan@kimtaewan-ui-Mac-mini.local>
2016-03-30 10:33:06 +0900

    0.1.6

--- Pod/TWScrollEmbedController.swift
+++ Pod/TWScrollEmbedController.swift
@@ -12,7 +12,7 @@ import UIKit
 public class TWScrollEmbedController: UIViewController {
     
     private var embedControllers = [UIViewController]()
-    private var currentController: UIViewController?
+    private(set) var currentController: UIViewController?
     
     public var useAnimation: Bool = true
     public var currentPage: Int = 0 {
